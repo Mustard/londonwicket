4b8dba903ff1200576f79f7764fe43ab305fa3d9
richardjohnwilkinson <richardjohnwilkinson@22a8d57d-b04e-0410-b501-9b3a71a140f9>
2009-11-22 14:59:16 +0000

    added visible and enabled conditions

--- Apps/wicket-component-expressions/src/main/java/com/googlecode/londonwicket/ComponentExpression.java
+++ Apps/wicket-component-expressions/src/main/java/com/googlecode/londonwicket/ComponentExpression.java
@@ -17,10 +17,13 @@ package com.googlecode.londonwicket;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
@@ -39,10 +42,8 @@ public class ComponentExpression {
 		}
 	}
 
-	public static Component findComponent(Component parent, String expression,
-			Class<? extends Component> typeRestriction) {
-		List<Component> results = findAllComponents(parent, expression,
-				typeRestriction);
+	public static Component findComponent(Component parent, String expression, Class<? extends Component> typeRestriction) {
+		List<Component> results = findAllComponents(parent, expression, typeRestriction);
 		if (results.isEmpty()) {
 			return null;
 		} else {
@@ -59,17 +60,12 @@ public class ComponentExpression {
 		if (expression == null || expression.equals("")) {
 			return Collections.emptyList();
 		}
-		return findComponent(parent, new LinkedList<String>(Arrays
-				.asList(expression.split(Character
-						.toString(Component.PATH_SEPARATOR)))), typeRestriction);
+		return findComponent(parent, new LinkedList<String>(Arrays.asList(expression.split(Character.toString(Component.PATH_SEPARATOR)))), typeRestriction);
 	}
 
-	private static List<Component> findComponent(Component parent,
-			LinkedList<String> expressionListIn,
-			Class<? extends Component> typeRestriction) {
+	private static List<Component> findComponent(Component parent, LinkedList<String> expressionListIn, Class<? extends Component> typeRestriction) {
 
-		LinkedList<String> expressionList = new LinkedList<String>(
-				expressionListIn);
+		LinkedList<String> expressionList = new LinkedList<String>(expressionListIn);
 
 		if (expressionList.isEmpty()) {
 			if (typeRestriction.isAssignableFrom(parent.getClass())) {
@@ -78,17 +74,19 @@ public class ComponentExpression {
 				return Collections.emptyList();
 			}
 		} else {
-			String first = expressionList.getFirst();
+			String rawFirst = expressionList.getFirst();
+			Collection<Condition> conditions = parseConditions(rawFirst);
+			final String first = rawFirst.replaceAll("\\s*\\[.*\\]\\s*", "");
+
 			if (!first.equals(ANY_COMPONENT_RECURSIVE_MATCHER)) {
 				expressionList.removeFirst();
-				List<Component> allMatches = getChild(parent, first);
+				List<Component> allMatches = getChild(parent, first, conditions);
 				if (allMatches.isEmpty()) {
 					return Collections.emptyList();
 				} else {
 					List<Component> finallyMatchedComponents = new ArrayList<Component>();
 					for (Component aMatch : allMatches) {
-						finallyMatchedComponents.addAll(findComponent(aMatch,
-								expressionList, typeRestriction));
+						finallyMatchedComponents.addAll(findComponent(aMatch, expressionList, typeRestriction));
 					}
 					return finallyMatchedComponents;
 				}
@@ -96,11 +94,11 @@ public class ComponentExpression {
 				List<Component> allMatches = new ArrayList<Component>();
 				if (parent instanceof MarkupContainer) {
 					for (Component aMatch : getAllChildren((MarkupContainer) parent)) {
-						if (typeRestriction.isAssignableFrom(aMatch.getClass())) {
+						if (typeRestriction.isAssignableFrom(aMatch.getClass())
+								&& evaluateConditions(conditions, aMatch)) {
 							allMatches.add(aMatch);
 						}
-						allMatches.addAll(findComponent(aMatch, expressionList,
-								typeRestriction));
+						allMatches.addAll(findComponent(aMatch, expressionList, typeRestriction));
 					}
 					return allMatches;
 				} else {
@@ -110,29 +108,36 @@ public class ComponentExpression {
 				List<Component> allMatches = new ArrayList<Component>();
 				LinkedList<String> fake = new LinkedList<String>();
 				fake.add(ANY_COMPONENT_RECURSIVE_MATCHER);
-				List<Component> allPotentialParents = findComponent(parent,
-						fake, Component.class);
+				List<Component> allPotentialParents = findComponent(parent, fake, Component.class);
 				expressionList.removeFirst();
 				for (Component aParent : allPotentialParents) {
-					allMatches.addAll(findComponent(aParent, expressionList,
-							typeRestriction));
+					if(evaluateConditions(conditions, aParent)){
+						allMatches.addAll(findComponent(aParent, expressionList, typeRestriction));
+					}
 				}
 				return allMatches;
 			}
 		}
 	}
 
-	private static List<Component> getChild(Component parent, String expression) {
+	private static List<Component> getChild(Component parent, String expression, Collection<Condition> conditions) {
 
 		if (parent instanceof MarkupContainer) {
-
 			MarkupContainer parentContainer = (MarkupContainer) parent;
 			if (expression.equals(ANY_COMPONENT_MATCHER)) {
-				return getAllChildren(parentContainer);
+				List<Component> allChildren = getAllChildren(parentContainer);
+				List<Component> allChildrenMatchingCondition = new ArrayList<Component>();
+				for (Component child : allChildren) {
+					if (child != null && evaluateConditions(conditions, child)) {
+						allChildrenMatchingCondition.add(child);
+					}
+				}
+				return allChildrenMatchingCondition;
 			} else {
 
 				Component comp = parentContainer.get(expression);
-				if (comp == null) {
+
+				if (comp == null || !evaluateConditions(conditions, comp)) {
 					return Collections.emptyList();
 				} else {
 					return Arrays.asList(comp);
@@ -140,6 +145,87 @@ public class ComponentExpression {
 			}
 		} else {
 			return Collections.emptyList();
+		}
+	}
+
+	private static final boolean evaluateConditions(Collection<Condition> conditions, Component component) {
+		boolean result = true;
+		for (Condition condition : conditions) {
+			switch (condition.getAttr()) {
+			case ENABLED: {
+				result = result && (component.isEnabledInHierarchy() == condition.getCondition());
+				break;
+			}
+			case VISIBLE: {
+				result = result && (component.isVisibleInHierarchy() == condition.getCondition());
+			}
+			}
+			if (!result) {
+				break;
+			}
+		}
+		return result;
+	}
+
+	private static final Collection<Condition> parseConditions(String expression) {
+		List<Condition> parsedConditions = new ArrayList<Condition>();
+		Pattern pattern = Pattern.compile(".*\\[\\s*(.*?)\\s*\\]\\w*", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(expression);
+		if (matcher.matches()) {
+			String conditionString = matcher.group(1);
+			String[] conditions = conditionString.split("\\s*(and|&)\\s*");
+			for (String aCondition : conditions) {
+				Condition.Attr attr = null;
+				Boolean condition = null;
+				String[] parts = null;
+				if (aCondition.contains("=")) {
+					parts = aCondition.split("\\s*=\\s*");
+					if (parts.length == 2) {
+						if (parts[0].toUpperCase().equals(Condition.Attr.VISIBLE.toString())) {
+							attr = Condition.Attr.VISIBLE;
+						} else if (parts[0].toUpperCase().equals(Condition.Attr.ENABLED.toString())) {
+							attr = Condition.Attr.ENABLED;
+						}
+						if (parts[1].toUpperCase().equals(Boolean.TRUE.toString().toUpperCase())) {
+							condition = Boolean.TRUE;
+						} else if (parts[1].toUpperCase().equals(Boolean.FALSE.toString().toUpperCase())) {
+							condition = Boolean.FALSE;
+						}
+						if (attr != null && condition != null) {
+							parsedConditions.add(new Condition(attr, condition));
+						}
+					}
+				}
+
+			}
+		}
+		return parsedConditions;
+	}
+
+	private static final class Condition {
+		private enum Attr {
+			VISIBLE, ENABLED
+		}
+
+		private final Attr attr;
+		private final boolean condition;
+
+		public Condition(Attr attr, boolean condition) {
+			this.attr = attr;
+			this.condition = condition;
+		}
+
+		public Attr getAttr() {
+			return attr;
+		}
+
+		public boolean getCondition() {
+			return condition;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("Condition [attr=%s, condition=%s]", attr, condition);
 		}
 	}
 
